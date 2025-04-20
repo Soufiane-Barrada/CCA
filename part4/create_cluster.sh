d714507dddf3523d3f5e1b79c5825856982dc474
Soufiane-Barrada <sb.soufiane.barrada@gmail.com>
2025-04-20 16:10:16 +0200

    me

--- part4/create_cluster.sh
+++ part4/create_cluster.sh
@@ -1,16 +1,15 @@
 #!/bin/bash 
 
-CLUSTER_NAME="part4.k8s.local"
-STATE_STORE="gs://cca-eth-2025-group-031-mpinto/"
-ZONE="europe-west1-b"
-export PROJECT=$(gcloud config get-value project)
-export KOPS_STATE_STORE=$STATE_STORE
+#CLUSTER_NAME="part4.k8s.local"
+#STATE_STORE="gs://cca-eth-2025-group-031-mpinto/"
+#ZONE="europe-west1-b"
+export PROJECT=`gcloud config get-value project`
+#export KOPS_STATE_STORE=$STATE_STORE
 
 kops create -f part4.yaml
-kops update cluster --name $CLUSTER_NAME --yes --admin 
+kops update cluster --name part4.k8s.local --yes --admin
 kops validate cluster --wait 10m
-sleep 10
 
 echo "[Matteo Log] cluster up!"
-kubectl get nodes -o wide 
+#kubectl get nodes -o wide 
 
