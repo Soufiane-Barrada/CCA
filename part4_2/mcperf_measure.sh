4a94191fe99dd4096c19bf6eae8d76a1f88e3602
Soufiane-Barrada <sb.soufiane.barrada@gmail.com>
2025-04-26 15:08:46 +0200

    part4 q2

---
+++ part4_2/mcperf_measure.sh
@@ -0,0 +1,7 @@
+source nodes_info.txt
+cd memcache-perf-dynamic
+./mcperf -s $MEMCACHED_IP --loadonly
+./mcperf -s $MEMCACHED_IP -a $CLIENT_AGENT_IP \
+--noload -T 8 -C 8 -D 4 -Q 1000 -c 8 -t 1200 \
+--qps_interval 10 --qps_min 5000 --qps_max 180000 \
+--qps_seed 8 > measure.txt
